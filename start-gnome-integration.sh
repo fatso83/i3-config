94081d69a2ec5e6b3b732211bc304a059a600f34
Carl-Erik Kopseng <carlerik@gmail.com>
2017-07-03 15:57:47 +0200

    Rename file

---
+++ start-gnome-integration.sh
@@ -0,0 +1,10 @@
+
+# /etc/X11/xinit/xinitrc.d/30-dbus     
+#!/bin/bash
+ # launches a session dbus instance
+ if [ -z "$DBUS_SESSION_BUS_ADDRESS" ] && type dbus-launch >/dev/null; then
+   eval $(dbus-launch --sh-syntax --exit-with-session)
+ fi
+
+eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
+export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
