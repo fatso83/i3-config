f7ecf52b9df933b6a37485174909c210cffe5a6b
Carl-Erik Kopseng <carlerik@gmail.com>
2017-07-03 17:40:14 +0200

    Further testing to get keyring working

--- start-gnome-integration.sh
+++ start-gnome-integration.sh
@@ -2,12 +2,12 @@
 # /etc/X11/xinit/xinitrc.d/30-dbus     
 #!/bin/bash
  # launches a session dbus instance
- if [ -z "$DBUS_SESSION_BUS_ADDRESS" ] && type dbus-launch >/dev/null; then
-   eval $(dbus-launch --sh-syntax --exit-with-session)
- fi
+ #if [ -z "$DBUS_SESSION_BUS_ADDRESS" ] && type dbus-launch >/dev/null; then
+   #eval $(dbus-launch --sh-syntax --exit-with-session)
+ #fi
 
 # gets me desktop icons in thunar, right scaling, etc
-gnome-settings-daemon
+# gnome-settings-daemon &
 
-eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
-export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
+#eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
+#export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
