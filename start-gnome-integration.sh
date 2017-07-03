b131577dc06749daeb489e1f21355aa1bf9d5141
Carl-Erik Kopseng <carlerik@gmail.com>
2017-07-03 16:08:44 +0200

    Use gnome-settings-daemon
    Fixes scaling, icons, etc

--- start-gnome-integration.sh
+++ start-gnome-integration.sh
@@ -6,5 +6,8 @@
    eval $(dbus-launch --sh-syntax --exit-with-session)
  fi
 
+# gets me desktop icons in thunar, right scaling, etc
+gnome-settings-daemon
+
 eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
